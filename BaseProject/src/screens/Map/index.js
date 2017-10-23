64e4127cc40a7576853d12159b46e99b4a262e9b
AnhPT <anhpt@ows.vn>
2017-10-23 17:26:39 +0700

    format mapview component

--- BaseProject/src/screens/Map/index.js
+++ BaseProject/src/screens/Map/index.js
@@ -1,7 +1,24 @@
 import * as React from "react";
-import { Container, Header, Body, Left, Icon, Title, Button, Text, Content, Form, Item, Input, Label, Right } from 'native-base';
-
+import { Container, Header, Body, Left, Icon, Title, Button, Content, Form, Item, Input, Label, Right } from 'native-base';
+import {
+    Text,
+    View,
+    Dimensions,
+    StyleSheet,
+} from 'react-native';
 import MapView from 'react-native-maps';
+
+import flagImg from '../../assets/images/flag-blue.png';
+
+const { width, height } = Dimensions.get('window');
+
+const ASPECT_RATIO = width / height;
+const LATITUDE = 37.78825;
+const LONGITUDE = -122.4324;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+const SPACE = 0.01;
+
 
 import styles from "./styles";
 export interface Props {
@@ -9,41 +26,66 @@ export interface Props {
 }
 export interface State { }
 class Map extends React.Component<Props, State> {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            region: {
+                latitude: LATITUDE,
+                longitude: LONGITUDE,
+            },
+        };
+    }
 
     render() {
         return (
-            <Container style={styles.container}>
-                <Header>
-                    <Body>
-                        <Title>Demo</Title>
-                    </Body>
-                </Header>
-                <Content>
-
-
-                    <MapView style={styles.map}
-                        initialRegion={{
-                            latitude: 37.78825,
-                            longitude: -122.4324,
-                            latitudeDelta: 0.0922,
-                            longitudeDelta: 0.0421,
-                        }}
-                        region={this.state.region}
-                    >
-                        <MapView.Marker
-                            coordinate={marker.latlng}
-                            title={marker.title}
-                            description={marker.description}
-                        />
-                    </MapView>
-
-
-                </Content>
-            </Container>
-
-
+            
+                    <View style={styles.container}>
+                        <MapView
+                            provider={this.props.provider}
+                            style={styles.map}
+                            initialRegion={this.state.region}
+                            loadingEnabled
+                            loadingIndicatorColor="#666666"
+                            loadingBackgroundColor="#eeeeee"
+                        >
+                            {/* <MapView.Marker
+                                coordinate={{
+                                    latitude: LATITUDE + SPACE,
+                                    longitude: LONGITUDE + SPACE,
+                                }}
+                                centerOffset={{ x: -18, y: -60 }}
+                                anchor={{ x: 0.69, y: 1 }}
+                                image={flagImg}
+                            /> */}
+                            <MapView.Marker
+                                coordinate={{
+                                    latitude: LATITUDE - SPACE,
+                                    longitude: LONGITUDE - SPACE,
+                                }}
+                                centerOffset={{ x: -42, y: -60 }}
+                                anchor={{ x: 0.84, y: 1 }}
+                            >
+                                <MapView.Callout>
+                                    <View>
+                                        <Text>This is a plain view</Text>
+                                    </View>
+                                </MapView.Callout>
+                            </MapView.Marker>
+                        </MapView>
+                        <View style={styles.buttonContainer}>
+                            <View style={styles.bubble}>
+                                <Text>Map with Loading</Text>
+                            </View>
+                        </View>
+                    </View>
+                
         );
     }
 }
+
+Map.propTypes = {
+    provider: MapView.ProviderPropType,
+};
 
 export default Map;
--- BaseProject/src/screens/Map/styles.js
+++ BaseProject/src/screens/Map/styles.js
@@ -18,6 +18,25 @@ const styles = StyleSheet.create({
     },
     map: {
         flex: 1
-    }
+	},
+	container: {
+        ...StyleSheet.absoluteFillObject,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+    map: {
+        ...StyleSheet.absoluteFillObject,
+    },
+    bubble: {
+        backgroundColor: 'rgba(255,255,255,0.7)',
+        paddingHorizontal: 18,
+        paddingVertical: 12,
+        borderRadius: 20,
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        marginVertical: 20,
+        backgroundColor: 'transparent',
+    },
 });
 export default styles;
