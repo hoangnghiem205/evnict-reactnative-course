b249fa210f679d01efa8b64231395706e593dcb5
AnhPT <anhpt@ows.vn>
2017-10-28 09:44:45 +0700

    add demo picker

--- BaseProject/src/screens/Home/index.js
+++ BaseProject/src/screens/Home/index.js
@@ -30,73 +30,13 @@ class Home extends React.Component<Props, State> {
     constructor(props) {
         super(props);
         this.state = {
-            selected1: "key1"
-        };
-    }
-
-    selectPhotoTapped() {
-        const options = {
-            quality: 1.0,
-            maxWidth: 500,
-            maxHeight: 500,
-            storageOptions: {
-                skipBackup: true
-            }
+            selected1: 1
         };
 
-        ImagePicker.showImagePicker(options, (response) => {
-            console.log('Response = ', response);
-
-            if (response.didCancel) {
-                console.log('User cancelled photo picker');
-            }
-            else if (response.error) {
-                console.log('ImagePicker Error: ', response.error);
-            }
-            else if (response.customButton) {
-                console.log('User tapped custom button: ', response.customButton);
-            }
-            else {
-                let source = { uri: response.uri };
-
-                // You can also display the image using data:
-                // let source = { uri: 'data:image/jpeg;base64,' + response.data };
-
-                this.setState({
-                    avatarSource: response
-                });
-            }
-        });
     }
-
-    onUploadImage() {
-        let source = this.state.avatarSource;
-        const data = new FormData();
-        data.append('photo', {
-            uri: source.uri,
-            type: source.type, // or photo.type
-            name: source.fileName
-        });
-
-        this.upoad(data)
-            .then((success) => {
-                console.log("onUploadImage success", success);
-            }
-            ).catch((error) => {
-                console.log("onUploadImage error", error);
-            });
-    }
-
-    upoad(formdata) {
-        console.log("onUploadImage:upoad to http://192.168.0.125:8000/upload data", formdata);
-        return fetch('http://192.168.0.125:8000/upload', {
-            method: 'POST',
-            headers: {
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
-            },
-            body: formdata
-        });
+    onValueChange(item){
+        console.log("onValueChange", item);
+        this.setState({selected1: item});
     }
 
     render() {
@@ -104,19 +44,22 @@ class Home extends React.Component<Props, State> {
             <Container>
                 <Header >
                     <Body>
-                        <Title>Image Demo</Title>
+                        <Title>Picker Demo</Title>
                     </Body>
                 </Header>
                 <Content contentContainerStyle={{ flex: 1 }} style={{ padding: 10 }}>
-                    <Grid style={{ alignItems: 'center' }}>
-                        <Col>
-                            <CardItem cardBody>
-                                <Image source={this.state.avatarSource} style={{ height: 200, width: null, flex: 1, marginBottom: 10 }} />
-                            </CardItem>
-                            <Button style={{ marginBottom: 20 }} block primary onPress={this.selectPhotoTapped.bind(this)}><Text> Choose Image </Text></Button>
-                            <Button block danger onPress={() => this.onUploadImage()}><Text> Upload Image </Text></Button>
-                        </Col>
-                    </Grid>
+                        <Picker
+                            iosHeader="Select one"
+                            mode="dropdown"
+                            selectedValue={this.state.selected1}
+                            onValueChange={this.onValueChange.bind(this)}
+                        >
+                            <Item label="Wallet" value={1}  key={1}/>
+                            <Item label="ATM Card" value={2}  key={2}/>
+                            <Item label="Debit Card" value={3} key={3}/>
+                            <Item label="Credit Card" value={4} key={4}/>
+                            <Item label="Net Banking" value={5} key={5}/>
+                        </Picker>
                 </Content>
             </Container>
         );
